f35525fa143c31c79cb004c3740f5aed031f9b27
Hope Giometti <hopegiometti@gmail.com>
2019-08-06 22:08:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,4 +18,18 @@ function divide(a, b) {
   number = a / b
   return number
 }
- 
+
+function inc(a) {
+  number = a + 1
+  return number
+}
+
+function dec(a) {
+  number = a - 1
+  return number
+}
+
+function makeInt(a) {
+  number = parseInt(a, 10)
+  return number
+}
