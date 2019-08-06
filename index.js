78384941c193e3accdc4588f334159f875da3195
Hope Giometti <hopegiometti@gmail.com>
2019-08-06 22:03:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,14 @@ function subtract(a, b) {
   number = a - b
   return number
 }
+
+function multiply(a, b) {
+  number = a * b 
+  return number
+}
+
+function divide(a, b) {
+  number = a / b
+  return number
+}
+ 
